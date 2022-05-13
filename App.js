32a86ca9b669097977df238f613adae479982fbc
p.jauk <p.jauk@mind-forge.at>
2022-05-13 18:26:29 +0200

    ....

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { StackNavigator } from "@react-navigation/stack/src/navigators/createStackNavigator";
+import { createStackNavigator} from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Home from './screens/Home';
+import Calendar from './screens/Calendar';
+import SetTimer from './screens/SetTimer';
+import Timer from './screens/Timer';
+import MeditationFinished from './screens/MeditatonFinished';
+import StreakSave from './screens/StreakSave';
+
+
+
+const NavStack = createStackNavigator();
+
+const App = () => {
+    return(
+        <NavigationContainer>
+            <NavStack.Navigator screenOptions={{ headerShown: false}} initialRouteName="Home">
+                <NavStack.Screen name="Home" component={ Home } />
+                <NavStack.Screen name="Meditate" component={ SetTimer} />
+                <NavStack.Screen name="Calendar" component={ Calendar } />
+                <NavStack.Screen name="StreakSave" component={ StreakSave } />
+                <NavStack.Screen name="Meditation Finished" component={ MeditationFinished } />
+                <NavStack.Screen name="Timer" component={ Timer } />
+            </NavStack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
